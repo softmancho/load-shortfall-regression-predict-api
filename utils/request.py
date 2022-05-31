e740eedb13c688c5ce7fb41482035ae9cf4638e3
Oswald Cedric SYENI <syoswaldcedric@gmail.com>
2022-05-30 21:50:23 -1200

    j

--- utils/request.py
+++ utils/request.py
@@ -30,7 +30,7 @@ test = pd.read_csv('./data/df_test.csv')
 
 # Convert our DataFrame to a JSON string.
 # This step is necessary in order to transmit our data via HTTP/S
-feature_vector_json = test.iloc[1].to_json
+feature_vector_json = test.iloc[1].to_json()
 
 # Specify the URL at which the API will be hosted.
 # NOTE: When testing your instance of the API on a remote machine
@@ -50,6 +50,6 @@ api_response = requests.post(url, json=feature_vector_json)
 # Display the prediction result
 print("Received POST response:")
 print("*"*50)
-print(f"API prediction result: {api_response.json()}")
+print(f"API prediction result: {api_response.json}")
 print(f"The response took: {api_response.elapsed.total_seconds()} seconds")
 print("*"*50)
